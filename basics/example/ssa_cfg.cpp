54940781e63f3a876a21da2e520010a6b0486bdc
Avaneethk <2023aa05604@wilp.bits-pilani.ac.in>
2026-06-22 14:51:09 +0530

    basicis

---
+++ basics/example/ssa_cfg.cpp
@@ -0,0 +1,14 @@
+/*
+
+LLVM IR hierarchy
+CFG
+Use-Def chains
+SSA
+Dominators
+Loop detection
+LLVM Analysis APIs
+CMake integration
+
+This dominator + back-edge concept is one of the most common LLVM optimization interview topics because passes like LICM, LoopRotate, IndVarSimplify, Vectorization, and Unrolling all depend on it.
+
+*/
